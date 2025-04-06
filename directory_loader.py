bcb4af1a5ba40722fa6d492c5c00ad630315570a
Sunilsangfroid <sunilsangfroid@gmail.com>
2025-04-06 06:57:38 +0530

    load and lazy_load features shown

--- directory_loader.py
+++ directory_loader.py
@@ -6,9 +6,15 @@ loader = DirectoryLoader(
     loader_cls=PyPDFLoader,
 )
 
-docs = loader.load()
+# docs = loader.load()
+docs = loader.lazy_load()
 
 # print(len(docs))
 
-print(docs[80].page_content)
-print(docs[80].metadata)
+# print(docs[80].page_content)
+# print(docs[80].metadata)
+
+for document in docs:
+    # print(document.page_content)
+    print(document.metadata)
+    print("\n")
